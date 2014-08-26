a6954bd8ed0e1efc250f2c4c64c9d464f09e7f70
Philip Helger <philip@helger.com>
2014-08-26 17:35:14 +0200

    Initial version

---
+++ src/main/java/com/google/javascript/jscomp/ClosureRunner.java
@@ -0,0 +1,157 @@
+/**
+ * Copyright (C) 2006-2014 phloc systems (www.phloc.com)
+ * Copyright (C) 2014 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.javascript.jscomp;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
+import javax.annotation.Nonnull;
+
+import org.apache.maven.plugin.logging.Log;
+
+import com.google.common.collect.Lists;
+
+/**
+ * The main class running the Closure compiler. It must reside in this package,
+ * as {@link AbstractCommandLineRunner} has only package visibility.
+ * 
+ * @author Philip Helger, phloc systems
+ */
+public final class ClosureRunner extends AbstractCommandLineRunner <Compiler, CompilerOptions>
+{
+  private final Log m_aLog;
+  private final String m_sCharset;
+
+  private static final boolean DEBUG = false;
+  private static final boolean GENERATE_EXPORTS = false;
+  private final CompilationLevel m_eLevel = CompilationLevel.SIMPLE_OPTIMIZATIONS;
+
+  public ClosureRunner (@Nonnull final Log aLog, @Nonnull final String sCharset)
+  {
+    if (aLog == null)
+      throw new NullPointerException ("log");
+    if (sCharset == null)
+      throw new NullPointerException ("charset");
+    m_aLog = aLog;
+    m_sCharset = sCharset;
+  }
+
+  @Override
+  protected Compiler createCompiler ()
+  {
+    return new Compiler (System.err);
+  }
+
+  @Override
+  protected List <SourceFile> createExterns () throws FlagUsageException, IOException
+  {
+    final List <SourceFile> externs = super.createExterns ();
+    // Use the default externs provided by the Closure CommandLineRunner
+    final List <SourceFile> defaultExterns = CommandLineRunner.getDefaultExterns ();
+    defaultExterns.addAll (externs);
+    return defaultExterns;
+  }
+
+  @Override
+  protected CompilerOptions createOptions ()
+  {
+    final CompilerOptions options = new CompilerOptions ();
+    options.setCodingConvention (new ClosureCodingConvention ());
+    final CompilationLevel level = m_eLevel;
+    level.setOptionsForCompilationLevel (options);
+    if (DEBUG)
+      level.setDebugOptionsForCompilationLevel (options);
+
+    if (GENERATE_EXPORTS)
+      options.setGenerateExports (GENERATE_EXPORTS);
+
+    WarningLevel.QUIET.setOptionsForWarningLevel (options);
+    options.prettyPrint = false;
+    options.printInputDelimiter = false;
+    options.closurePass = false;
+    return options;
+  }
+
+  private void _setDefaultConfig ()
+  {
+    getCommandLineConfig ().setPrintTree (false)
+                           .setPrintAst (false)
+                           .setPrintPassGraph (false)
+                           .setJscompDevMode (CompilerOptions.DevMode.OFF)
+                           .setLoggingLevel (Level.WARNING.getName ())
+                           .setExterns (Lists.<String> newArrayList ())
+                           .setJs (Lists.<String> newArrayList ())
+                           .setJsOutputFile ("")
+                           .setModule (Lists.<String> newArrayList ())
+                           .setVariableMapInputFile ("")
+                           .setPropertyMapInputFile ("")
+                           .setVariableMapOutputFile ("")
+                           .setCreateNameMapFiles (false)
+                           .setPropertyMapOutputFile ("")
+                           .setCodingConvention (true ? CodingConventions.getDefault ()
+                                                     : new ClosureCodingConvention ())
+                           .setSummaryDetailLevel (1)
+                           .setOutputWrapper ("")
+                           .setModuleWrapper (Lists.<String> newArrayList ())
+                           .setModuleOutputPathPrefix ("./")
+                           .setCreateSourceMap ("")
+                           .setDefine (Lists.<String> newArrayList ())
+                           .setCharset (m_sCharset)
+                           .setManageClosureDependencies (false)
+                           .setClosureEntryPoints (Lists.<String> newArrayList ())
+                           .setOutputManifest (Lists.<String> newArrayList ())
+                           .setAcceptConstKeyword (false)
+                           .setLanguageIn ("ECMASCRIPT5");
+  }
+
+  public boolean compressJSFile (@Nonnull final File aSourceFile,
+                                 @Nonnull final File aDestFile,
+                                 @Nonnull final File [] aExterns)
+  {
+    if (aSourceFile == null)
+      throw new NullPointerException ("sourceFile");
+
+    try
+    {
+      // Build result file name
+      m_aLog.info ("Compressing JS " + aSourceFile.getName () + " to " + aDestFile.getName ());
+
+      final List <String> aExternList = new ArrayList <String> ();
+      for (final File f : aExterns)
+        aExternList.add (f.getAbsolutePath ());
+
+      _setDefaultConfig ();
+      getCommandLineConfig ().setExterns (aExternList)
+                             .setJs (Lists.newArrayList (aSourceFile.getAbsolutePath ()))
+                             .setJsOutputFile (aDestFile.getAbsolutePath ());
+
+      final int nErrors = doRun ();
+      if (nErrors == 0)
+        return true;
+      m_aLog.error ("Failed to compress JS with " + nErrors + " errors: " + aSourceFile.getName ());
+    }
+    catch (final Exception ex)
+    {
+      m_aLog.error ("Failed to compress JS " + aSourceFile.toString (), ex);
+    }
+    return false;
+  }
+}
---
+++ src/main/java/com/helger/maven/jscompress/JSCompressMojo.java
@@ -0,0 +1,195 @@
+/**
+ * Copyright (C) 2006-2014 phloc systems (www.phloc.com)
+ * Copyright (C) 2014 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.maven.jscompress;
+
+import java.io.File;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+
+import com.google.javascript.jscomp.ClosureRunner;
+
+/**
+ * @author Philip Helger, phloc systems
+ * @goal compressjs
+ * @phase generate-resources
+ * @description Compress existing JS file using the Google Closure compressor.
+ */
+public final class JSCompressMojo extends AbstractMojo
+{
+  private static final String EXTENSION_JS = ".js";
+  private static final String [] EXTENSIONS_JS_COMPRESSED = new String [] { ".min.js",
+                                                                           "-min.js",
+                                                                           ".minified.js",
+                                                                           "-minified.js" };
+
+  /**
+   * The Maven Project.
+   * 
+   * @parameter property="project"
+   * @required
+   * @readonly
+   */
+  private MavenProject project;
+
+  /**
+   * The directory where the JS files reside. It must be an existing directory.
+   * 
+   * @required
+   * @parameter property="sourceDirectory"
+   *            default-value="${basedir}/src/main/resources"
+   */
+  private File sourceDirectory;
+
+  /**
+   * Should the system properties be emitted as well?
+   * 
+   * @parameter property="recursive" default-value="true"
+   */
+  private boolean recursive = true;
+
+  /**
+   * The encoding of the source JS files.
+   * 
+   * @parameter property="sourceEncoding" default-value="UTF-8"
+   */
+  private String sourceEncoding = "UTF-8";
+
+  /**
+   * The extension that should be supplied to the minified/compressed JS file.
+   * 
+   * @parameter property="targetFileExtension" default-value=".min.js"
+   */
+  private String targetFileExtension = ".min.js";
+
+  /**
+   * Should the system properties be emitted as well?
+   * 
+   * @parameter property="forceCreation" default-value="false"
+   */
+  private boolean forceCreation = false;
+
+  public void setProject (final MavenProject aProject)
+  {
+    project = aProject;
+  }
+
+  public void setSourceDirectory (final File aDir)
+  {
+    sourceDirectory = aDir;
+    if (!sourceDirectory.isAbsolute ())
+      sourceDirectory = new File (project.getBasedir (), aDir.getPath ());
+    if (!sourceDirectory.exists ())
+      getLog ().error ("JS source directory '" + sourceDirectory + "' does not exist!");
+  }
+
+  public void setRecursive (final boolean bRecursive)
+  {
+    recursive = bRecursive;
+  }
+
+  public void setSourceEncoding (final String sSourceEncoding)
+  {
+    sourceEncoding = sSourceEncoding;
+  }
+
+  public void setTargetFileExtension (final String sTargetFileExtension)
+  {
+    targetFileExtension = sTargetFileExtension;
+  }
+
+  public void setForceCreation (final boolean bForceCreation)
+  {
+    forceCreation = bForceCreation;
+  }
+
+  /**
+   * Check if the passed file is already compressed. The check is only done
+   * using the file extension of the file name.
+   * 
+   * @param sFilename
+   *        The filename to be checked.
+   * @return <code>true</code> if the file is already compressed.
+   */
+  private static boolean _isAlreadyCompressed (final String sFilename)
+  {
+    for (final String sExt : EXTENSIONS_JS_COMPRESSED)
+      if (sFilename.endsWith (sExt))
+        return true;
+    return false;
+  }
+
+  @Nullable
+  public static String getWithoutExtension (@Nullable final String sFilename)
+  {
+    if (sFilename == null)
+      return null;
+
+    final int nExtensionIndex = sFilename.lastIndexOf ('.');
+    final int nLastSepIndex = Math.max (sFilename.lastIndexOf ('/'), sFilename.lastIndexOf ('\\'));
+    final int nIndex = nLastSepIndex > nExtensionIndex ? -1 : nExtensionIndex;
+    return nIndex == -1 ? sFilename : sFilename.substring (0, nIndex);
+  }
+
+  private void _compressJSFile (@Nonnull final File aChild, @Nonnull final ClosureRunner aRunner)
+  {
+    // Compress the file only if the compressed file is older than the original
+    // file. Note: lastModified on a non-existing file returns 0L
+    final File aCompressed = new File (getWithoutExtension (aChild.getAbsolutePath ()) + targetFileExtension);
+    if (forceCreation || aCompressed.lastModified () < aChild.lastModified ())
+    {
+      getLog ().debug ("Start " + (forceCreation ? "forced " : "") + "compressing JS file " + aChild.toString ());
+      aRunner.compressJSFile (aChild, aCompressed, new File [0]);
+    }
+    else
+      getLog ().debug ("Ignoring already compressed JS file " + aChild.toString ());
+  }
+
+  private void _scanDirectory (@Nonnull final File aDir, @Nonnull final ClosureRunner aRunner)
+  {
+    final File [] aChildren = aDir.listFiles ();
+    if (aChildren != null)
+      for (final File aChild : aChildren)
+      {
+        if (aChild.isDirectory ())
+        {
+          // Shall we recurse into sub-directories?
+          if (recursive)
+            _scanDirectory (aChild, aRunner);
+        }
+        else
+          if (aChild.isFile () &&
+              aChild.getName ().endsWith (EXTENSION_JS) &&
+              !_isAlreadyCompressed (aChild.getName ()))
+          {
+            // We're ready to rumble!
+            _compressJSFile (aChild, aRunner);
+          }
+      }
+  }
+
+  public void execute () throws MojoExecutionException
+  {
+    final ClosureRunner aRunner = new ClosureRunner (getLog (), sourceEncoding);
+    _scanDirectory (sourceDirectory, aRunner);
+  }
+}
